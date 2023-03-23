2ee8f42f798cf2006248d9e14b25cc9f34ee26f1
ваше_имя <grigorenko-marko@rambler.ru>
2023-03-23 21:42:28 +0300

    task 3 correkt

--- task003/Program.cs
+++ task003/Program.cs
@@ -11,47 +11,31 @@ if (a > 0 && a < 8)
     {
         System.Console.WriteLine("Понедельник");
     }
-    else
-    {
-        if (a == 2)
+    else if (a == 2)
     {
         System.Console.WriteLine("Вторник");
     }
-    else
-    {
-        if (a == 3)
+    else if (a == 3)
     {
         System.Console.WriteLine("Среда");
     }
-    else
-    {
-        if (a == 4)
+    else if (a == 4)
     {
         System.Console.WriteLine("Четверг");
     }
-    if (a == 5)
+    else if (a == 5)
     {
         System.Console.WriteLine("Пятница");
     }
-    else
-    {
-        if (a == 6)
+    else if (a == 6)
     {
         System.Console.WriteLine("Суббота");
     }
-    else
-    {
-        if (a == 7)
+    else if (a == 7)
     {
         System.Console.WriteLine("Воскресенье");
     }
     }
-    }
-    }
-    }
-    }
-    
-}
 else 
 {
     System.Console.WriteLine("неверные данные");
